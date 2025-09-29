60b9ec01e9676dde43040778b79ea5586979f687
Kashi Kweyu <kashiku789@gmail.com>
2025-09-29 17:23:15 +0300

    chore: add .gitignore, README skeleton, env example

---
+++ src/app/legal/terms/page.tsx
@@ -0,0 +1,8 @@
+export default function TermsPage() {
+  return (
+    <article className="prose dark:prose-invert max-w-none">
+      <h1>Terms & Conditions</h1>
+      <p>Use of this website constitutes acceptance of these terms.</p>
+    </article>
+  )
+}
